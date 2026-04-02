147793c06e97714a81e08d40f9e6cdc0f5b80720
Rodrigo Rodrigues <rodrigo.rsmail@gmail.com>
2026-04-02 16:20:44 -0300

    feat(common): add Container component

---
+++ src/components/common/Container/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function Container({ children, className = "" }: Props) {
+  return <div className={`${styles.container} ${className}`}>{children}</div>;
+}
