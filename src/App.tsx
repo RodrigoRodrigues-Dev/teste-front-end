f16dbf0eb9e4a45c40efce88cec3e94e17056830
Rodrigo Rodrigues <rodrigo.rsmail@gmail.com>
2026-04-03 14:57:50 -0300

    feat(app): compose home page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <h1>Hello, World!</h1>;
-}
+import Categories from "./components/sections/Categories";
+import Header from "./components/layout/Header";
+import HomeHero from "./components/sections/HomeHero";
+import Showcase from "./components/sections/Showcase";
+import Partners from "./components/sections/Partners";
+import BrowseBrands from "./components/sections/BrowseBrands";
+import Newsletter from "./components/sections/Newsletter";
+import Footer from "./components/layout/Footer";
+
+const App = () => (
+  <>
+    <Header />
+    <main>
+      <HomeHero />
+      <Categories />
+      <Showcase primary />
+      <Partners />
+      <BrowseBrands />
+      <Showcase secondary />
+      <Newsletter />
+    </main>
+    <Footer />
+  </>
+);
 
 export default App;
